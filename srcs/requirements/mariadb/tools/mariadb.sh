4bc018f40b6a9ce03a44a4d8b2dfd6e3146308af
Ikrame Assafe <iassafe@e3r5p1.1337.ma>
2024-09-16 17:05:35 +0100

    l

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,15 +1,64 @@
 #!/bin/bash
 
 
+
+# 1 start mariadb ...
 service mariadb start
+
+
+
+
 sleep 5
-mysql -u root --skip-password <<EOF
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PASSWORD');
-CREATE DATABASE IF NOT EXISTS mariadbDataBase;
-CREATE USER IF NOT EXISTS 'MariadbUser'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';
-GRANT ALL PRIVILEGES ON mariadbDataBase.* TO 'MariadbUser'@'%';
-FLUSH PRIVILEGES;
-EOF
+
+
+# 2 configuration ...
+
+mysqladmin -u root password $MARIADB_ROOT_PASSWORD
+
+mysql -u root -p$MARIADB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE ;"
+mysql -u root -p$MARIADB_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD' ;"
+mysql -u root -p$MARIADB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%' ;"
+mysql -u root -p$MARIADB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+
+
+# 3 stop the service and run the container in  ...
 service mariadb stop
 
+# start the MySQL server daemon
+# bind-address where to accept conection ....
 mysqld_safe --port=3306 --bind-address=0.0.0.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #!/bin/bash
+
+
+# service mariadb start
+# sleep 5
+# mysql -u root --skip-password <<EOF
+# SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PASSWORD');
+# CREATE DATABASE IF NOT EXISTS mariadbDataBase;
+# CREATE USER IF NOT EXISTS 'MariadbUser'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';
+# GRANT ALL PRIVILEGES ON mariadbDataBase.* TO 'MariadbUser'@'%';
+# FLUSH PRIVILEGES;
+# EOF
+# service mariadb stop
+
+# mysqld_safe --port=3306 --bind-address=0.0.0.0
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,4 +1,57 @@
 #!/bin/sh
+
+# sleep 10
+
+
+
+
+
+# service php7.4-fpm start
+
+# #for wordpress files ...
+# mkdir -p /var/www/html
+# cd /var/www/html
+
+# wp core download --allow-root
+# wp config create --allow-root --dbname=$MARIADB_DATABASE --dbuser=$WP_ADMIN_LOGIN --dbpass=$WP_ADMIN_PASSWORD --dbhost=$DB_HOST
+# wp core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_LOGIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+
+
+# wp user create --allow-root $WP_USER_LOGIN $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=$WP_USER_ROLE
+
+
+# service php7.4-fpm stop
+
+# php-fpm7.4 -F
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #!/bin/sh
 # Wait for dependent services to be ready (e.g., MariaDB)
 sleep 10
 
@@ -9,7 +62,7 @@ mkdir -p /var/www/html
 cd /var/www/html
 
 # Download WordPress core files if not already present
-if [ ! -f wp-config.php ]; then
+# if [ ! -f wp-config.php ]; then
     wp core download --allow-root
 
     # Create wp-config.php file with database details
@@ -30,10 +83,11 @@ if [ ! -f wp-config.php ]; then
     # Create a new WordPress user
     wp user create --allow-root "$WP_USER_LOGIN" "$WP_USER_EMAIL" \
             --user_pass="$WP_USER_PASSWORD" \
-            --role="$WP_USER_ROLE"
-fi
+            --role=author
+# fi
 
 
 # Stop PHP-FPM service and run it in the foreground
 service php7.4-fpm stop
 php-fpm7.4 -F
+
